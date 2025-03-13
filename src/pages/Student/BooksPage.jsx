726feaf5209b2b6bbb72344be33d443aaf7ea80c
Naren404 <narendrasunar47@gmail.com>
2025-03-13 21:15:10 +1100

    logout feature

--- src/pages/Student/BooksPage.jsx
+++ src/pages/Student/BooksPage.jsx
@@ -1,10 +1,16 @@
 import { Container } from "react-bootstrap";
+import Header from "../../components/Header";
 
 const BooksPage = () => {
-  return ( 
-    <Container>
-      <h2>List Of Books</h2>
-    </Container>
+  return (
+    <>
+      <Header />
+
+      <Container>
+        <h2>List Of Books</h2>
+      </Container>
+    </>
+    
    );
 }
  
